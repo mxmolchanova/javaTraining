293ccca038d019ae7d856b91867fe30baf542fec
DSK14 DSK <DSK14@ptc.local>
2018-08-21 13:10:09 +0300

    Implementation of methods task1

--- src/com/molcmar/task/Methods.java
+++ src/com/molcmar/task/Methods.java
@@ -17,4 +17,41 @@ public class Methods {
         }
         return count;
     }
+
+    public Object getFirstElement(List<String> list) {
+        if (list.isEmpty()) {
+            return 0;
+        }
+        return list.get(0);
+    }
+
+    public Object getLastElement(List<String> list) {
+        if (list.isEmpty()) {
+            return 0;
+        }
+        return list.get(list.size()-1);
+    }
+
+    public Object getEqualElement(List<String> list, String e) {
+        if (list.isEmpty()) {
+            return 0;
+        }
+
+        for (int i = 0; i < list.size(); i++) {
+            String value = list.get(i);
+            if (e.equals(value)) {
+                return value;
+            }
+        }
+        return 0;
+    }
+
+    public Object getValueOf(List<String> list, int index) {
+        if (index < list.size()) {
+            return list.get(index);
+        }
+        return 0;
+    }
+
+
 }
--- test/com/molcmar/task/MethodsTest.java
+++ test/com/molcmar/task/MethodsTest.java
@@ -1,8 +1,11 @@
 package com.molcmar.task;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -12,19 +15,75 @@ import static org.junit.Assert.*;
  */
 public class MethodsTest {
     private Methods methods = new Methods();
+    List<String> list;
+
+    @Before
+    public void initializeList() {
+        list = Arrays.asList("a1","a2","a3", "a1");
+
+    }
+
 
     @Test
     public void testCountA1(){
-        List<String> list = new ArrayList<>();
-        list.add("a1");
-        list.add("a2");
-        list.add("a3");
-        list.add("a1");
+//        List<String> list = new ArrayList<>();
+//        list.add("a1");
+//        list.add("a2");
+//        list.add("a3");
+//        list.add("a1");
 
         int expectedCount = 2;
         int actualCount = methods.countA1(list);
 
         assertEquals(expectedCount, actualCount);;
     }
+
+    @Test
+    public void testGetFirstElement() {
+
+        List<String> list1 = new ArrayList<>();
+
+        Object expectedValue = 0;
+        Object actualValue = methods.getFirstElement(list1);
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void testGetLastElement() {
+
+//        List<String> list1 = new ArrayList<>();
+
+        Object expectedValue = "a1";
+        Object actualValue = methods.getLastElement(list);
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void testGetEqualElement() {
+
+//        List<String> list1 = new ArrayList<>();
+
+        Object expectedValue = "a3";
+        Object actualValue = methods.getEqualElement(list, "a3");
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+    @Test
+    public void testGetValueOf() {
+
+//        List<String> list1 = new ArrayList<>();
+
+        Object expectedValue = "a3";
+        Object actualValue = methods.getValueOf(list, 2);
+        assertEquals(expectedValue, actualValue);
+
+    }
+
+
+
+
 
 }
